ae82f379867b6514674cd204cabb490b60f4957a
Nithin Prathapan <nithinprathapn3256@gmail.com>
2022-12-04 20:52:09 +0530

    added styles to cart component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import "./styles.css";
 const Navbar = () => {
   const [nav, setNav] = useState(false);
   return (
     <div className="w-full max-w[1240px] h-24 items-center px-4 justify-between flex text-[#fee] mx-auto font-bold font-sans">
       <h1 className="p-3.8 m-4 text-[#00df9a] text-3xl">CART-BAE</h1>
       <ul className="uppercase md:flex hidden">
-        <li className="p-4 flex font-thin">Home</li>
-        <li className="p-4 flex font-thin">About</li>
-        <li className="p-4 flex font-thin">Company</li>
-        <li className="p-4 flex font-thin">Resources</li>
-        <li className="p-4 flex font-thin">Signin</li>
+        <li className="link-underline pl-2 pr-2   flex font-thin">Home</li>
+        <li className="link-underline pl-2 pr-2 flex font-thin">About</li>
+        <li className="link-underline pl-2 pr-2 flex font-thin">Company</li>
+        <li className="link-underline pl-2 pr-2 flex font-thin">Resources</li>
+        <li className="link-underline pl-2 pr-2 flex font-thin">Signin</li>
       </ul>
       <div onClick={() => setNav(!nav)} className="block md:hidden">
         {!nav ? <AiOutlineMenu size={20} /> : <AiOutlineClose size={25} />}
