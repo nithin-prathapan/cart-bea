34ad0a3a94b7b65fd711ea2012212be95d3c64ac
Nithin Prathapan <nithinprathapn3256@gmail.com>
2022-12-04 14:47:50 +0530

    Navbar is added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,36 @@
-import React from "react";
-import { AiOutlineMenu } from "react-icons/ai";
+import React, { useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
   return (
     <div className="w-full max-w[1240px] h-24 items-center px-4 justify-between flex text-[#fee] mx-auto font-bold font-sans">
-      <h1 className="p-4 m-4 text-[#00df9a] text-3xl">CART-BAE</h1>
-      <ul className="uppercase flex md:hidden">
+      <h1 className="p-3.8 m-4 text-[#00df9a] text-3xl">CART-BAE</h1>
+      <ul className="uppercase md:flex hidden">
         <li className="p-4 flex font-thin">Home</li>
         <li className="p-4 flex font-thin">About</li>
         <li className="p-4 flex font-thin">Company</li>
         <li className="p-4 flex font-thin">Resources</li>
         <li className="p-4 flex font-thin">Signin</li>
       </ul>
-      <div>
-        <AiOutlineMenu size={20} />
+      <div onClick={() => setNav(!nav)} className="block md:hidden">
+        {!nav ? <AiOutlineMenu size={20} /> : <AiOutlineClose size={25} />}
       </div>
-      <div className="fixed left-0 top-0 w-[30%] border-r border-r-grey-900 h-full bg-[#000300] hidden">
+      <div
+        className={
+          nav
+            ? "fixed left-0 top-0 w-[45%] md:border-none border-r border-r-grey-900 h-full bg-[#000300] ease-in-out duration-200"
+            : "fixed left-[-100%]"
+        }
+      >
         <h1 className="p-4 text-[#00df9a] text-3xl m-4">CART-BAE</h1>
-        <ul className="uppercase pt-24">
+        <ul className="uppercase  justify-center mx-auto block px-4 md:hidden">
           <li className="p-4 font-thin border-b border-b-gray-500">Home</li>
           <li className="p-4 font-thin border-b border-b-gray-500">About</li>
           <li className="p-4 font-thin border-b border-b-gray-500">Company</li>
           <li className="p-4 font-thin border-b border-b-gray-500">
             Resources
           </li>
-          <li className="p-4 font-thin">Signin</li>
+          <li className="p-4 font-thin">Sign-in</li>
         </ul>
       </div>
     </div>
