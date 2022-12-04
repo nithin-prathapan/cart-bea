cdddc68961bce1aea569563eca28abafd25fce67
Nithin Prathapan <nithinprathapn3256@gmail.com>
2022-12-04 10:09:59 +0530

    test

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+const Navbar = () => {
+  return (
+    <div className="w-full max-w[1240px] h-24 items-center px-4 justify-between flex text-[#fee] mx-auto font-bold font-sans">
+      <h1 className="p-4 m-4 text-[#00df9a] text-3xl">CART-BAE</h1>
+      <ul className="uppercase flex md:hidden">
+        <li className="p-4 flex font-thin">Home</li>
+        <li className="p-4 flex font-thin">About</li>
+        <li className="p-4 flex font-thin">Company</li>
+        <li className="p-4 flex font-thin">Resources</li>
+        <li className="p-4 flex font-thin">Signin</li>
+      </ul>
+      <div>
+        <AiOutlineMenu size={20} />
+      </div>
+      <div className="fixed left-0 top-0 w-[30%] border-r border-r-grey-900 h-full bg-[#000300] hidden">
+        <h1 className="p-4 text-[#00df9a] text-3xl m-4">CART-BAE</h1>
+        <ul className="uppercase pt-24">
+          <li className="p-4 font-thin border-b border-b-gray-500">Home</li>
+          <li className="p-4 font-thin border-b border-b-gray-500">About</li>
+          <li className="p-4 font-thin border-b border-b-gray-500">Company</li>
+          <li className="p-4 font-thin border-b border-b-gray-500">
+            Resources
+          </li>
+          <li className="p-4 font-thin">Signin</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
